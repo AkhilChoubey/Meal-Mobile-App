c5cc8a406cbd46449eb7ac075c9eb9d098756b03
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-10-26 01:30:26 +0530

    Home page

--- App.js
+++ App.js
@@ -7,18 +7,13 @@ import AppLoading from 'expo-app-loading';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={}>
+      
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });
